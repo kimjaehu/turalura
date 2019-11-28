2f0e0bf96839e6af9718693f2e42ed400322a03b
kimjaehu <kimjaehu@gmail.com>
2019-11-27 20:16:45 -0500

    added stream to progress view

--- lib/views/progress_view.dart
+++ lib/views/progress_view.dart
@@ -1,73 +1,94 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:turalura/views/new_measurement/measurement_view.dart';
 import 'package:turalura/models/Measurement.dart';
 import 'package:turalura/widgets/growth_graph.dart';
+import 'package:turalura/widgets/provider_widget.dart';
 
 class ProgressView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final newMeasurement = new Measurement(null, null, null, null, null);
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 50.0,
-            child: RaisedButton(
-              child: Text("data"),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => NewMeasurementView(
-                            measurement: newMeasurement,
-                          )),
-                );
-              },
-            ),
-          ),
-          Expanded(
-            child: DefaultTabController(
-              length: 3,
-              initialIndex: 0,
-              child: Column(
-                children: <Widget>[
-                  TabBar(
-                    tabs: <Widget>[
-                      Tab(
-                        text: 'Height',
+    return StreamBuilder<Object>(
+      stream: null,
+      builder: (context, snapshot) {
+        return Container(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    child: Text("data"),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => NewMeasurementView(
+                                  measurement: newMeasurement,
+                                )),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: DefaultTabController(
+                  length: 3,
+                  initialIndex: 0,
+                  child: Column(
+                    children: <Widget>[
+                      TabBar(
+                        tabs: <Widget>[
+                          Tab(
+                            text: 'Height',
+                          ),
+                          Tab(
+                            text: 'Weight',
+                          ),
+                          Tab(
+                            text: 'Data',
+                          ),
+                        ],
                       ),
-                      Tab(
-                        text: 'Weight',
-                      ),
-                      Tab(
-                        text: 'Data',
-                      ),
+                      Expanded(
+                        child: TabBarView(
+                          children: <Widget>[
+                            Center(
+                              child: GrowthChart(
+                                title: "Height-for-age",
+                              ),
+                            ),
+                            Center(
+                              child: GrowthChart(
+                                title: "Weight-for-age",
+                              ),
+                            ),
+                            Center(child: Text('Summary')),
+                          ],
+                        ),
+                      )
                     ],
                   ),
-                  Expanded(
-                    child: TabBarView(
-                      children: <Widget>[
-                        Center(
-                          child: GrowthChart(
-                            title: "Height-for-age",
-                          ),
-                        ),
-                        Center(
-                          child: GrowthChart(
-                            title: "Weight-for-age",
-                          ),
-                        ),
-                        Center(child: Text('Summary')),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
     );
+  }
+    Stream<QuerySnapshot> getUserBabySummaryStreamSnapshots(
+      BuildContext context, userSnapshot) async* {
+    final uid = await Provider.of(context).auth.getCurrentUID();
+    String currentBaby = userSnapshot.data['currentBaby'];
+    print(currentBaby);
+    yield* Firestore.instance
+        .collection('summaries')
+        .document(uid)
+        .collection(currentBaby.toLowerCase())
+        .snapshots();
   }
 }
